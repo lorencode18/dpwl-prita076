8e4824fae7a47045fd5c2913485acc5cc57ab0a5
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2026-09-21 17:38:54 +0700

    menambahkan kode yang memuat field

---
+++ model/Latihan1Model.php
@@ -0,0 +1,12 @@
+<?php
+class Latihan1Model
+{
+    public function getAllMhs()
+    {
+        return [
+            ['nama' => 'Catherine', 'nim' => '3022501'],
+            ['nama' => 'Prita', 'nim' => '3022502'],
+            ['nama' => 'Anjelita', 'nim' => '3022503']
+        ];
+    }
+}
