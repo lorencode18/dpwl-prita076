bd56f916d83df34574167122f2dafb16d006dacb
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2026-09-21 17:34:50 +0700

    menambahkan kode agar semua request masuk sebelum ke controller

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+<?php
+require_once 'config/routes.php';
+$url = $_GET['url'] ?? '';
+if ($url === '') {
+    $url = $route['default_controller'] . '/index';
+}
 
+$url = trim($url, '/');
+$segment = explode('/', $url);
+$controller = $segment[0] ?? $route['default_controller'];
+$method     = $segment[1] ?? 'index';
+$parameter  = $segment[2] ?? null;
