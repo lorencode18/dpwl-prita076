afc7049c201d5201ef6952c2b61a495fdf7a5916
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2026-09-21 17:49:59 +0700

    Menambahkan kode untuk routing dan pemanggilan controller

--- index.php
+++ index.php
@@ -10,3 +10,21 @@ $segment = explode('/', $url);
 $controller = $segment[0] ?? $route['default_controller'];
 $method     = $segment[1] ?? 'index';
 $parameter  = $segment[2] ?? null;
+
+$controllerName = ucfirst($controller);
+$controllerFile = 'controller/' . $controllerName . '.php';
+if (file_exists($controllerFile)) {
+    require_once $controllerFile;
+    $objController = new $controllerName();
+    if (method_exists($objController, $method)) {
+        if ($parameter !== null) {
+            $objController->$method($parameter);
+        } else {
+            $objController->$method();
+        }
+    } else {
+        echo "Method tidak ditemukan.";
+    }
+} else {
+    echo "Controller tidak ditemukan.";
+}
