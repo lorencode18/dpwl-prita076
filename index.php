3f3647b7f47a369ccfb98801736578f9ae04e10f
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2026-09-22 13:29:23 +0700

    Menambahkan config dan URL helper ke index

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 require_once 'config/routes.php';
+require_once 'config/cofig.php';
+require_once 'config/routes.php';
+require_once 'config/url_helper.php';
+
 $url = $_GET['url'] ?? '';
 if ($url === '') {
     $url = $route['default_controller'] . '/index';
