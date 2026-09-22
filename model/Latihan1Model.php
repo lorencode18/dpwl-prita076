30ce2f84bfe4f2a4c39e8b705d8216821d7b0077
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2026-09-22 13:37:11 +0700

    Menambah field alamat dan no_telp

--- model/Latihan1Model.php
+++ model/Latihan1Model.php
@@ -4,9 +4,9 @@ class Latihan1Model
     public function getAllMhs()
     {
         return [
-            ['nama' => 'Catherine', 'nim' => '3022501'],
-            ['nama' => 'Prita', 'nim' => '3022502'],
-            ['nama' => 'Anjelita', 'nim' => '3022503']
+            ['nim' => '3022501', 'nama' => 'Fakhril', 'alamat' => 'Jl. Pangkalpinang bangka', 'no_telp' => '0891928398'],
+            ['nim' => '3022502', 'nama' => 'Rani', 'alamat' => 'Jl. Sungailiat Bangka', 'no_telp' => '092887487'],
+            ['nim' => '3022503', 'nama' => 'Dresta', 'alamat' => 'Jl. Ciledug Tangerang', 'no_telp' => '081737638']
         ];
     }
 }
