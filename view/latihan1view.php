d9ac6e012622517074d6fba7b60cc2d18b367f5c
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2026-09-22 13:40:37 +0700

    Menambahkan kolom alamat dan no_telp

--- view/latihan1view.php
+++ view/latihan1view.php
@@ -7,11 +7,26 @@
 </head>
 <body>
     <h2>Daftar Mahasiswa</h2>
-    <?php
-    $i = 1;
-    foreach ($datamhs as $mhs) {
-        echo $i++ . '. ' . $mhs['nama'] . ' ' . $mhs['nim'] . '<br>';
-    }
-    ?>
+    <table border="1" cellpadding="3" cellspacing="0">
+         <tr>
+            <th>NO.</th>
+            <th>NIM</th>
+            <th>NAMA MAHASISWA</th>
+            <th>ALAMAT</th>
+            <th>NO.TELP</th>
+        </tr>
+        <?php
+        $i = 1;
+        foreach ($datamhs as $mhs) {
+            echo '<tr>';
+            echo '<td>' . $i++ . '.</td>';
+            echo '<td>' . $mhs['nim'] . '</td>';
+            echo '<td>' . $mhs['nama'] . '</td>';
+            echo '<td>' . $mhs['alamat'] . '</td>';
+            echo '<td>' . $mhs['no_telp'] . '</td>';
+            echo '</tr>';
+        }
+        ?>
+    </table>
 </body>
 </html>
